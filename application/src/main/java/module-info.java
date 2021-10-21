fb52026a55d29d3bb96c14372c91564f8130ffdd
joshuas029 <jsamontanez@knights.ucf.edu>
2021-10-21 17:56:30 -0400

    initial commit

---
+++ application/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.application {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
+
+    opens com.application to javafx.fxml;
+    exports com.application;
+}
